225f19220b4af86316b2ec792fa47c36c4c93fed
kevin henderson <umhendek@gmail.com>
2026-08-17 19:01:45 -0600

    Restore user customizations: English, lights on, OpenFreeMap tiles, simple header, own metadata

--- src/themes/classic/components/Header/index.tsx
+++ src/themes/classic/components/Header/index.tsx
@@ -1,11 +1,8 @@
-import { Link } from 'react-router-dom';
 import type { ReactElement } from 'react';
-import getSiteMetadata from '@core/hooks/useSiteMetadata';
 import { useTheme, Theme } from '../../hooks/useTheme';
 import styles from './style.module.css';
 
 const Header = () => {
-  const { logo, siteUrl, navLinks } = getSiteMetadata();
   const { theme, setTheme } = useTheme();
 
   const icons: Record<Theme, ReactElement> = {
@@ -53,24 +50,8 @@ const Header = () => {
 
   return (
     <>
-      <nav className="mx-auto mt-4 flex w-full max-w-screen-2xl min-w-max items-center justify-between pl-6 lg:px-8">
-        <div className="w-1/4">
-          <Link to={siteUrl}>
-            <picture>
-              <img className="h-16 w-16 rounded-full" alt="logo" src={logo} />
-            </picture>
-          </Link>
-        </div>
-        <div className="flex w-3/4 items-center justify-end text-right">
-          {navLinks.map((n) => (
-            <a
-              key={n.url}
-              href={n.url}
-              className="mr-3 text-lg lg:mr-4 lg:text-base"
-            >
-              {n.name}
-            </a>
-          ))}
+      <nav className="mx-auto mt-12 flex w-full max-w-screen-2xl min-w-max items-center justify-end pl-6 lg:px-16">
+        <div className="flex items-center justify-end text-right">
           <div className="ml-4 flex items-center space-x-2">
             <button
               type="button"
--- src/themes/classic/utils/const.ts
+++ src/themes/classic/utils/const.ts
@@ -1,9 +1,6 @@
 // Constants
-const MAPBOX_TOKEN =
-  // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
-  // Instead, manually add a new token and apply URL restrictions.
-  // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJjbWYxdXR4YncwMTJtMm5zOTE4eTZpMGdtIn0.OnsXdwkZFztR8a5Ph_T-xg';
+// The map tiles are provided by MapCN (Carto basemaps) by default and do not require a token.
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN || '';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -41,18 +38,18 @@ const ROAD_LABEL_DISPLAY = true;
 const PRIVACY_MODE = false;
 // updated on 2024/11/17: lights are turned off by default
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
-const LIGHTS_ON = false;
+const LIGHTS_ON = true;
 //set to `true` if you want to show the 'Elevation Gain' column
 const SHOW_ELEVATION_GAIN = true;
 // richer title for the activity types (like garmin style)
 const RICH_TITLE = false;
 
 // IF you are outside China please make sure IS_CHINESE = false
-const IS_CHINESE = true;
+const IS_CHINESE = false;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  return `记录自己骑行 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Cycling Journey with ${yearLength} Years, the table shows year ${year} data`;
@@ -226,7 +223,7 @@ export const INDOOR_COLOR = '#8899aa';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = 'mapcn';
+export const MAP_TILE_VENDOR = 'mapcn_openfreemap';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
 export const MAP_TILE_STYLE_LIGHT = 'osm-bright';
